ba865376a8eceab14edfae022d15a910907536d5
preferrrr <zzangsh2946@naver.com>
2023-07-07 15:14:32 +0900

    first commit

---
+++ src/main/java/com/example/codemate/CodeMateApplication.java
@@ -0,0 +1,13 @@
+package com.example.codemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codemate/CodeMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
