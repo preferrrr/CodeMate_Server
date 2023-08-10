cb0ccc04f9fc93b6edcf2aa5fb3479670923942e
preferrrr <zzangsh2946@naver.com>
2023-08-10 17:15:01 +0900

    feat : ocr 요청 추가

---
+++ src/main/java/com/example/codemate/controller/OcrController.java
@@ -0,0 +1,28 @@
+package com.example.codemate.controller;
+
+import com.example.codemate.service.OcrService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequestMapping("/ocr")
+@Controller
+@CrossOrigin("*")
+@RequiredArgsConstructor
+public class OcrController {
+
+    private final OcrService ocrService;
+    @PostMapping("")
+    public ResponseEntity<?> OCRtest(@RequestPart MultipartFile file) throws IOException {
+
+        String result = ocrService.ocr(file);
+
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/example/codemate/service/OcrService.java
@@ -0,0 +1,71 @@
+package com.example.codemate.service;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
+
+@Service
+public class OcrService {
+
+    private static String IMAGE_FILE_PATH = "/home/ubuntu/ImageFiles/";
+    private static String OCR_DATA_PATH = "/home/ubuntu/Ocr/";
+
+    //private static String IMAGEFILE_PATH = "C:\\intellij_project\\capstone_image\\";
+    public String ocr(MultipartFile file) throws IOException{
+
+        /**요청으로 받은 이미지 저장*/
+        String filename = UUID.randomUUID().toString() + file.getOriginalFilename().substring(file.getOriginalFilename().length()-4, file.getOriginalFilename().length());
+
+        String imageFilePath = IMAGE_FILE_PATH + filename;
+
+        try {
+            Path destination = new File(imageFilePath).toPath();
+            Files.copy(file.getInputStream(), destination, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new IOException();
+        }
+        /**요청으로 받은 이미지 저장 완료*/
+
+        String result = ocrImage(imageFilePath); // 문자 인식
+
+        return result;
+    }
+
+
+    private Tesseract getTesseract() {
+        Tesseract instance = new Tesseract();
+        instance.setDatapath(OCR_DATA_PATH);
+        instance.setLanguage("eng+kor");//"kor+eng"
+        return instance;
+    }
+
+    public String ocrImage(String filePath) {
+
+        Tesseract tesseract = getTesseract();
+
+        String result = null;
+
+        File file = new File(filePath);
+
+        if(file.exists() && file.canRead()) {
+            try {
+                result = tesseract.doOCR(file);
+            } catch (TesseractException e) {
+                result = e.getMessage();
+            }
+        } else {
+            result = "not exist";
+        }
+        return result;
+
+    }
+}
